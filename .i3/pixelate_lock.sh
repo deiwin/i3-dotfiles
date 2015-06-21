132da6305a3865a8f97062ad062cc5a6e6813739
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-06-21 21:51:06 +0300

    add set -e for the pixelate lock script

--- .i3/pixelate_lock.sh
+++ .i3/pixelate_lock.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f ~/.i3/lock-icon.png ]] && convert /tmp/screen.png ~/.i3/lock-icon.png -gravity center -composite -matte /tmp/screen.png
